0334d011e7b38608d2d5a0a327c97a749286564f
24750Montenegro <mon24750@uvg.edu.gt>
2026-07-12 12:00:00 -0600

    feat: agrega polígono 2 azul con orilla blanca

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,19 @@ fn main() {
     fb.set_current_color(Color::WHITE);
     draw_polygon(&mut fb, &poligono1);
 
+    // Poligono 2
+    let poligono2 = [
+        Vector2::new(321.0, 335.0),
+        Vector2::new(288.0, 286.0),
+        Vector2::new(339.0, 251.0),
+        Vector2::new(374.0, 302.0),
+    ];
+
+    fb.set_current_color(Color::BLUE);
+    fill_polygon(&mut fb, &poligono2);
+    fb.set_current_color(Color::WHITE);
+    draw_polygon(&mut fb, &poligono2);
+
     fb.render_to_file("out.png");
     fb.render_to_file("out.bmp");
     println!("Imagenes generadas: out.png, out.bmp");
