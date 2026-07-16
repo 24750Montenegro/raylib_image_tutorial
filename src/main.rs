025b54629c2b3814eeaccd6c75149d3d9f7ba739
24750Montenegro <mon24750@uvg.edu.gt>
2026-07-16 10:00:00 -0600

    feat: dibuja polígono 3 rojo con orilla blanca

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,22 @@ mod fill;
 
 use raylib::prelude::*;
 use framebuffer::Framebuffer;
+use draw_polygon::draw_polygon;
+use fill::fill_polygon;
 
 fn main() {
     let mut fb = Framebuffer::new(800, 600, Color::BLACK);
+
+    let poligono3 = [
+        Vector2::new(377.0, 249.0),
+        Vector2::new(411.0, 197.0),
+        Vector2::new(436.0, 249.0),
+    ];
+
+    fb.set_current_color(Color::RED);
+    fill_polygon(&mut fb, &poligono3);
+    fb.set_current_color(Color::WHITE);
+    draw_polygon(&mut fb, &poligono3);
 
     fb.render_to_file("out.png");
     fb.render_to_file("out.bmp");
